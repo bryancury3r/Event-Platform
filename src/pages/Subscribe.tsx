a29e609a177cacc9842e59be074b2820444e976e
bryancury3r <gabriel_bryancury@outlook.com>
2022-07-04 20:31:27 -0300

    styles

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -6,6 +6,14 @@ export function Subscribe() {
       <div className="max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
           <Logo />
+
+          <h1 className="mt-8 text-[2.5rem] leading-tight">
+            Construa uma aplicação completa, do zero, com React JS
+          </h1>
+
+          <p className="mt-4 text-gray-200 leading-relaxed">
+            Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
+          </p>
         </div>
 
       </div>
