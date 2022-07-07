6daf22c5d9324e8af510c27be29d5a448056610a
bryancury3r <gabriel_bryancury@outlook.com>
2022-07-07 20:30:59 -0300

    add style slug

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -18,13 +18,15 @@ export function Lesson(props: LesseonProps) {
     locale: ptBR,
   })
 
+  const isActiveLesson = slug === props.slug;
+
   return (
     <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">
         {availableDateFormat}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' : ''}`}>
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
